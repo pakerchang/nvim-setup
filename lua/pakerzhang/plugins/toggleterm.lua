f876166f0e5e418b71fff818a4dfb6e5db51e3cb
pzdesu <pakerchang@gmail.com>
2023-03-13 01:15:10 +0800

    :bulb: Update.

--- lua/pakerzhang/plugins/toggleterm.lua
+++ lua/pakerzhang/plugins/toggleterm.lua
@@ -7,16 +7,23 @@ toggleterm.setup({
   size = 20,
   open_mapping = [[<leader>ti]],
   hide_numbers = true,
-  direction = "float",
-  float_ops = {},
+  direction = "horizontal", -- "horizontal" | "tab" | "float"
   insert_mappings = true,
   start_in_insert = true,
+  close_on_exit = true,
+  float_ops = {
+    border = "curved",
+    winblend = 0,
+    highlights = {
+      border = "Normal",
+      background = "Normal",
+    },
+  },
 })
 
 function _G.set_terminal_keymaps()
   local opts = { noremap = true }
-
-  vim.api.buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], opts)
+  vim.api.buf_set_keymap(0, "t", "<ESC>", [[<C-\><C-n><C-\>]], opts)
   vim.api.buf_set_keymap(0, "t", "jk", [[<C-\><C-n>]], opts)
   vim.api.buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
   vim.api.buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
