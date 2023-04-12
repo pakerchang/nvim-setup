5ea07484fb1ca07880b60c601d278c528a2299f1
pzdesu <pakerchang@gmail.com>
2023-04-12 13:39:40 +0800

    docs(Settings): adjust toggle terminal keycuts.

--- lua/pakerzhang/plugins/toggleterm.lua
+++ lua/pakerzhang/plugins/toggleterm.lua
@@ -5,7 +5,7 @@ end
 
 toggleterm.setup({
   size = 20,
-  open_mapping = [[<leader>ti]],
+  open_mapping = [[<leader>tei]],
   hide_numbers = true,
   direction = "float", -- "horizontal" | "tab" | "float"
   insert_mappings = true,
