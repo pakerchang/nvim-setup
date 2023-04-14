4305d5307a059d0ddfcf93da45f7d54e803f2fa9
pzdesu <pakerchang@gmail.com>
2023-04-14 19:44:01 +0800

    feat(Settings): change leader key, toggle terminal key, sort key pending time.

--- lua/pakerzhang/plugins/toggleterm.lua
+++ lua/pakerzhang/plugins/toggleterm.lua
@@ -5,7 +5,7 @@ end
 
 toggleterm.setup({
   size = 20,
-  open_mapping = [[<leader>ti]],
+  open_mapping = [[<leader>tmi]],
   hide_numbers = true,
   direction = "horizontal", -- "horizontal" | "tab" | "float"
   insert_mappings = true,
